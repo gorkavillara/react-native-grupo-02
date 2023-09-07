5d6c58820d827294dfe0a79b525d7059191232cb
gorkavillara <gorkavillara@gmail.com>
2023-09-07 17:02:32 +0200

    ⚛️ Mi primera app con Expo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => Alert.alert("Has hecho click");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto} onPress={handlePress}>
+        ¡Hola mundo!
+      </Text>
+      <Button title="Botón" onPress={() => Alert.alert("Has hecho click")} />
+      <TouchableOpacity onPress={() => null}>
+        <Text
+          style={{
+            fontSize: 24,
+            paddingVertical: 8,
+            paddingHorizontal: 16,
+            backgroundColor: "#dd0088",
+            color: "white",
+            borderRadius: 8,
+          }}
+        >
+          Otro botón
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  texto: { fontSize: 32, marginVertical: 16 },
 });
